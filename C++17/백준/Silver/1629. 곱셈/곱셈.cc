957553fd80b7bbb1fdd270b504c8f7457cdf8916
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-03-17 13:25:05 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+long long Multi(long long iA, long long iB, long long iC)
+{
+	if (iB == 0)
+		return 1;
+	else if (iB == 1)
+		return iA % iC;
+
+	int iResult = Multi(iA, iB / 2, iC);
+
+	if (iB % 2 == 1)
+		return iA % iC * iResult % iC * iResult % iC;
+	else
+		return iResult % iC * iResult % iC;
+}
+
+int main(void* pArg)
+{
+	long long iA, iB, iC;
+
+	cin >> iA >> iB >> iC;
+
+	long long iResult = Multi(iA, iB, iC);
+
+	cout << iResult << "\n";
+
+	return 0;
+}
