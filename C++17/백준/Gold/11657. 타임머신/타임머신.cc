83146c8bc16d67e581ad7a309fe20afcccbca51c
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-03-18 15:43:49 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2220 KB -BaekjoonHub

--- C++17/백준/Gold/11657. 타임머신/타임머신.cc
+++ C++17/백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,82 +1,77 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <queue>
+#include <map>
+#include <algorithm>
 
-#define DIST_MAX 987654321
 using namespace std;
 
-int iN{}, iE{};
-long long iMinDist[501] = {};
+int iN, iM;
 
 vector<pair<pair<int, int>, int>> vecEdges = {};
 
-void Find_Path(int iStartNode)
-{
-	iMinDist[iStartNode] = 0;
-	// 노드 N-1 개만큼 반복. (노드가 N개인 그래프에서 한 경로의 최대 간선 갯수는 N-1개 이므로.)
-	// 약간 BFS 느낌?
-	for (int i = 0; i < iN - 1; ++i)
-	{
-		//모든 간선을 순회하면서, 방문 가능한지 체크.
-		for (auto& iter : vecEdges)
-		{
-			// 만약 방문할 노드의 거리가 갱신되어있지 않다면 생략.
-			if (iMinDist[iter.first.first] == DIST_MAX)
-				continue;
-
-			// 현재 노드에서 다른 노드로 가는 최소 거리의 갱신이 가능하다면,
-			if (iMinDist[iter.first.second] > iMinDist[iter.first.first] + iter.second)
-			{
-				// 갱신.
-				iMinDist[iter.first.second] = iMinDist[iter.first.first] + iter.second;
-			}
-		}
-	}
-}
+long long iDist[501] = {};
 
 int main(void* pArg)
 {
-	ios::sync_with_stdio();
-	cin.tie(NULL);
+	cin >> iN >> iM;
 
-	cin >> iN >> iE;
-
-	for (int i = 0; i < iE; ++i)
+	for (int i = 0; i < iM; ++i)
 	{
-		int iSour{}, iDest{}, iCost{};
-	
+		int iSour, iDest, iCost;
+
 		cin >> iSour >> iDest >> iCost;
 
-		vecEdges.emplace_back(make_pair(make_pair(iSour, iDest), iCost));
+		vecEdges.push_back(make_pair(make_pair(iSour, iDest), iCost));
 	}
 
 	for (int i = 0; i <= 500; ++i)
 	{
-		iMinDist[i] = DIST_MAX;
+		iDist[i] = 987654321;
 	}
 
-	Find_Path(1);
+	iDist[1] = 0;
 
-	for (auto& iter : vecEdges)
+	for (int i = 0; i < iN - 1; ++i)
 	{
-		if (iMinDist[iter.first.first] == DIST_MAX)
-			continue;
-
-		if (iMinDist[iter.first.second] > iMinDist[iter.first.first] + iter.second)
+		for (auto& iter : vecEdges)
 		{
-			cout << "-1\n";
-			return 0;
+			if (iDist[iter.first.first] == 987654321)
+				continue;
+
+			if (iDist[iter.first.second] > iDist[iter.first.first] + iter.second)
+			{
+				iDist[iter.first.second] = iDist[iter.first.first] + iter.second;
+			}
 		}
 	}
 
-	for (int i = 2; i <= iN; ++i)
+	bool hasCycle = false;
+	for (auto& iter : vecEdges)
 	{
-		if (iMinDist[i] == DIST_MAX)
-			cout << "-1\n";
-		else
-			cout << iMinDist[i] << "\n";
+		if (iDist[iter.first.first] == 987654321)
+			continue;
+
+		if (iDist[iter.first.second] > iDist[iter.first.first] + iter.second)
+		{
+			hasCycle = true;
+		}
 	}
 
+	if(true == hasCycle)
+		cout << "-1";
+
+	else
+	{
+		for (int i = 2; i <= iN; ++i)
+		{
+			if (iDist[i] == 987654321)
+				cout << "-1\n";
+			else
+				cout << iDist[i] << "\n";
+		}
+	}
 
 	return 0;
 }
