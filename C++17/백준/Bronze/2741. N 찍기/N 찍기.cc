a4791e1a5c58cf16004d76f59b0b50b063184de4
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-03-24 15:53:54 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main(void* pArg)
+{
+	int iN = {};
+
+	cin >> iN;
+
+	for (int i = 1; i <= iN; ++i)
+	{
+		cout << i << "\n";
+	}
+
+	return 0;
+}
