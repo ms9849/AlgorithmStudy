1d876d83977bf8c2a20300769099a0c3aa37d371
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-06-05 19:58:44 +0900

    [level 1] Title: 모의고사, Time: 0.04 ms, Memory: 4 MB -BaekjoonHub

--- C++/프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ C++/프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -4,40 +4,44 @@
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
-	vector<int> Result = {};
-
-	vector<int> FirstAnswer = { 1,2,3,4,5 };
-	vector<int> SecondAnswer = { 2,1,2,3,2,4,2,5 };
-	vector<int> ThridAnswer = { 3,3,1,1,2,2,4,4,5,5 };
-
-	int iFirstCount{}, iSecondCount{}, iThirdCount{};
-
-	int iCount = 0;
-	for (auto& iter : answers)
-	{
-		if (answers[iCount] == FirstAnswer[iCount % FirstAnswer.size()])
-			iFirstCount++;
-
-		if (answers[iCount] == SecondAnswer[iCount % SecondAnswer.size()])
-			iSecondCount++;
-
-		if (answers[iCount] == ThridAnswer[iCount % ThridAnswer.size()])
-			iThirdCount++;
-
-		iCount++;
-	}
-
-	int iMax = -1;
-	iMax = max(max(iFirstCount, iSecondCount), iThirdCount);
-
-	if (iFirstCount == iMax)
-		Result.push_back(1);
-
-	if (iSecondCount == iMax)
-		Result.push_back(2);
-
-	if (iThirdCount == iMax)
-		Result.push_back(3);
-
-	return Result;
+    vector<int> answer;
+    
+    //1번 수포자
+    vector<int> vecFirst = { 1,2,3,4,5};   
+    int iAnswerFirst = {};
+    //2번 수포자
+    vector<int> vecSecond = {2,1,2,3,2,4,2,5};
+        int iAnswerSecond = {};
+    //3번 수포자
+    vector<int> vecThird = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+        int iAnswerThird = {};
+    
+    for(int i=0; i<answers.size(); ++i)
+    {
+        if(answers[i] == vecFirst[i % vecFirst.size()])
+        {
+            iAnswerFirst++;
+        }
+        if(answers[i] == vecSecond[i % vecSecond.size()])
+        {
+            iAnswerSecond++;
+        }
+        
+        if(answers[i] == vecThird[i % vecThird.size()])
+        {
+            iAnswerThird++;
+        }
+    }
+    
+    int iMax = iAnswerFirst > iAnswerSecond ? iAnswerFirst : iAnswerSecond;
+    iMax = iMax > iAnswerThird ? iMax : iAnswerThird;
+    
+    if(iMax == iAnswerFirst)
+        answer.push_back(1);    
+    if(iMax == iAnswerSecond)
+        answer.push_back(2);    
+    if(iMax == iAnswerThird)
+        answer.push_back(3);
+    
+    return answer;
 }
