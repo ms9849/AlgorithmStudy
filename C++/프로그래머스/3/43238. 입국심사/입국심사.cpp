9867ba5ec1ac0a19237a6518d278bcbfa1cb64a2
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-06-05 17:55:33 +0900

    [level 3] Title: 입국심사, Time: 21.06 ms, Memory: 6.88 MB -BaekjoonHub

--- C++/프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ C++/프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,70 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <map>
-#include <algorithm>
 #include <string>
-#include <list>
-#include "limits.h"
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-/*
-Right 범위는 맞음..
-그럼 Left는 최소값인데 범위가?..
-*/
-long long solution(int n, vector<int> times) {
-	long long answer = 0;
-	long long iMax = 0;
-	long long iMin = INT_MAX;
-
-	for (auto& iter : times)
-	{
-		if (iter > iMax)
-			iMax = iter;
-
-		if (iter < iMin)
-			iMin = iter;
-	}
-
-	long long iLow = iMin;
-	long long iHigh = iMax * n;
-
-
-	long long iMid = {};
-
-	while (iLow <= iHigh)
-	{
-		iMid = (iLow + iHigh) / 2;
-
-		long long iCount = 0;
-		for (auto& iter : times)
-			iCount += iMid / iter;
-
-		//엥 더 채울수있네? 그럼 더 낮춰봐
-		if (iCount >= n)
-		{
-			answer = iMid;
-			iHigh = iMid - 1;
-		}
-		//사람 다 못채우면 시간 늘려야지
-		else
-		{
-			iLow = iMid + 1;
-		}
-
-	}
-
-	return answer;
-}
-
-int main(void* pArg)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	solution(6, {7, 10});
-	return 0;
+long long solution(int iNumber, vector<int> vecTimes) {
+    long long iAnswer = 0;
+    long long iLow = {};
+    long long iMid = {};
+    long long iHigh = {};
+    
+    sort(vecTimes.begin(), vecTimes.end());
+    
+    iLow = vecTimes[0] * (iNumber / vecTimes.size());
+    iHigh = vecTimes[vecTimes.size()-1] * iNumber;
+    
+    while(iLow <= iHigh)
+    {
+        // overflow 방지.
+        iMid = (iLow + iHigh)/2;
+        
+        long long iCanHandle = {};
+        
+        for(auto& iter : vecTimes)
+        {
+            iCanHandle += iMid / iter;
+        }
+        
+        if(iCanHandle >= iNumber)
+        {
+            iHigh = iMid-1;
+        }
+        else if(iCanHandle < iNumber)
+        {
+            iLow = iMid+1;
+        }
+    }
+    
+    
+    return iLow;
 }
