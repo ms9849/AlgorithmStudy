d78658e7fc3083bf1850b0d9c27bb7ecddf4d936
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-06-05 19:03:15 +0900

    [level 1] Title: 최소직사각형, Time: 0.37 ms, Memory: 5.08 MB -BaekjoonHub

--- C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -4,30 +4,29 @@
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-	int answer = 0;
-
-	for (auto& iter : sizes)
-	{
-		if (iter[0] < iter[1])
-		{
-			int iTemp = iter[1];
-			iter[1] = iter[0];
-			iter[0] = iTemp;
-		}
-	}
-
-	int iMaxWidth = 0;
-	int iMaxHeight = 0;
-
-	for (int i = 0; i < sizes.size(); ++i)
-	{
-		if (sizes[i][0] > iMaxWidth)
-			iMaxWidth = sizes[i][0];
-
-		if (sizes[i][1] > iMaxHeight)
-			iMaxHeight = sizes[i][1];
-	}
-
-	return iMaxHeight * iMaxWidth;
+    
+    for(auto& iter : sizes)
+    {
+        //Swap 알고리즘.
+        //더 긴 길이가 0번째 인덱스에 오도록 처리한다.
+        if(iter[1] > iter [0])
+        {
+            int iTemp = iter[0];
+            iter[0] = iter[1];
+            iter[1] = iTemp;
+        }
+    }
+    
+    int iWidthMax = {};
+    int iHeightMax = {};
+    
+    for(auto& iter : sizes)
+    {
+        if(iter[0] > iWidthMax)
+            iWidthMax = iter[0];
+        if(iter[1] > iHeightMax)
+            iHeightMax = iter[1];
+    }
+    
+    return iWidthMax * iHeightMax;
 }
-
