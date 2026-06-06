ebb5526075a823f092dec8275ee8506a0a78ccdb
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-06-06 13:52:05 +0900

    [level 2] Title: 피로도, Time: 0.43 ms, Memory: 4.25 MB -BaekjoonHub

--- C++/프로그래머스/2/87946. 피로도/피로도.cpp
+++ C++/프로그래머스/2/87946. 피로도/피로도.cpp
@@ -3,63 +3,52 @@
 
 using namespace std;
 
-int iVisited[9] = {};
-int iDungeonCnt = {};
-int iIdx = {};
-int iResult = {};
-vector<vector<int>> DungeonInfos = {};
+//가거나, 가지 않거나..
+//최대가 나오려면 계속 탐사를 하긴 해야겠지?..
+//dp로도 풀이 가능해보이지만 완탐이 가장 직관적일 것.
 
-void Recursion(int iCurK, int iCurrentCount)
+int iVisited[10] = {};
+int iMaxDungeons = {};
+int iNumDungeons = {};
+vector<vector<int>> vecDungeons = {};
+/* 종료 조건 */
+// 1. 모든 던전을 탐험함
+// 2. 더 이상 피로도 조건에 맞는 던전이 없음.
+
+void Recursion(int iCurPower, int iCurDungeons)
 {
-	if (iCurrentCount == iDungeonCnt)
-	{
-		int iTemp = 0;
-		for (int i = 0; i < iDungeonCnt; ++i)
-		{
-			if (iVisited[i] == 1)
-			{
-				iTemp++;
-			}
-		}
-
-		if (iTemp > iResult)
-			iResult = iTemp;
-
-		return;
-	}
-
-	for (int i = 0; i < iDungeonCnt; ++i)
-	{
-		if (iVisited[i] == 0)
-		{
-			if (iCurK >= DungeonInfos[i][0])
-			{
-				iVisited[i] = 1;
-				iCurK -= DungeonInfos[i][1];
-
-				Recursion(iCurK, iCurrentCount + 1);
-
-				iVisited[i] = 0;
-				iCurK += DungeonInfos[i][1];
-			}
-			else
-			{
-				iVisited[i] = 2;
-				Recursion(iCurK, iCurrentCount + 1);
-				iVisited[i] = 0;
-			}
-		}
-	}
+    bool isRaidable = { false };
+    for(int i=0; i<iNumDungeons; ++i)
+    {
+        //최소 필요 피로도
+        int iRequirePower = vecDungeons[i][0];
+        //사용 피로도
+        int iUsePower = vecDungeons[i][1]; 
+        
+        if(iVisited[i] == 0 &&
+          iCurPower >= iRequirePower &&
+          iCurPower >= iUsePower)
+        {
+            isRaidable = true;
+            
+            iVisited[i] = 1;
+            Recursion(iCurPower - iUsePower, iCurDungeons + 1);
+            iVisited[i] = 0;
+        }
+    }
+    
+    if(false == isRaidable)
+    {
+        iMaxDungeons = iMaxDungeons > iCurDungeons ? iMaxDungeons : iCurDungeons;
+    }
 }
+
 int solution(int k, vector<vector<int>> dungeons) {
-	int answer = -1;
-
-	DungeonInfos = dungeons;
-	iDungeonCnt = dungeons.size();
-
-	Recursion(k, 0);
-
-	answer = iResult;
-
-	return answer;
+    int iAnswer = { -1 };
+    vecDungeons = dungeons;
+    iNumDungeons = dungeons.size();
+    
+    Recursion(k, 0);
+    
+    return iMaxDungeons;
 }
