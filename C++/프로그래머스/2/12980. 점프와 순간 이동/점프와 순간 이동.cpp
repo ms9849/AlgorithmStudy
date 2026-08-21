b3d6cebd797c64e028db4f71c5041bc0c887641b
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-21 16:09:03 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 4.75 MB -BaekjoonHub

--- C++/프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ C++/프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <queue>
 using namespace std;
-
 
 int solution(int n)
 {
-	int iCount = 0;
+    int ans = 0;
+    
+    while(n != 0)
+    {   
+        if(n%2 == 0)
+            n = n/2;
+        else
+        {
+            n--;
+            ans++;
+        }
+    }
 
-	while (n != 0)
-	{
-		if (n % 2 == 0)
-			n /= 2;
-		else
-		{
-			n -= 1;
-			iCount++;
-		}
-	}
-
-	return iCount;
+    return ans;
 }
