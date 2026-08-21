c4f22999071bf5cd245bbd3d92c3400d677fd3bb
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-21 13:54:12 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ C++/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -4,37 +4,29 @@
 using namespace std;
 
 string solution(string s) {
-
-	string answer = "";
-	bool isFirstCase = { true };
-
-	for (int i = 0; i < s.length(); ++i)
-	{
-		if (i == 0 || s[i - 1] == ' ')
-			isFirstCase = true;
-		else
-			isFirstCase = false;
-
-		if (true == isFirstCase)
-		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				answer += s[i] + 'A' - 'a';
-			}
-			else
-				answer += s[i];
-		}
-		else
-		{
-			if (s[i] >= 'A' && s[i] <= 'Z')
-			{
-				answer += s[i] - ('A' - 'a');
-			}
-			else
-				answer += s[i];
-		}
-	}
-
-
-	return answer;
+    string answer = "";
+    
+    int iSize = s.length();
+    
+    for(int i=0; i<iSize; ++i)
+    {
+        if(i == 0)
+        {
+            if(s[i] >= 'a' && s[i] <='z')
+                answer += s[i] - 32;
+            else
+                answer += s[i];
+        }
+        else
+        {
+            if(s[i] >= 'a' && s[i] <= 'z' && s[i-1] == ' ')
+                answer += s[i] - 32;
+            else if(s[i] >= 'A' && s[i] <='Z' && s[i-1] != ' ')
+                answer += s[i] + 32;
+            else
+                answer += s[i];
+        }
+    }
+    
+    return answer;
 }
