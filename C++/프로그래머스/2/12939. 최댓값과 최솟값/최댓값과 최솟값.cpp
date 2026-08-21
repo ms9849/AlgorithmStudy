6f37f7d0551587cf06db8af4ca997ace7ce733e0
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-21 13:40:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.02 ms, Memory: 4.82 MB -BaekjoonHub

--- C++/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ C++/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -3,33 +3,36 @@
 #include <algorithm>
 
 using namespace std;
-
-string solution(string s) {
-	string answer = "";
-	 
-	string strTemp = "";
-	vector<int> vecNums = {};
-
-	for (int i = 0; i < s.length(); ++i)
-	{
-		if (s[i] == ' ')
-		{
-			vecNums.push_back(stoi(strTemp));
-			strTemp = "";
-		}
-		else
-		{
-			strTemp += s[i];
-		}
-	}
-
-	vecNums.push_back(stoi(strTemp));
-
-	sort(vecNums.begin(), vecNums.end());
-
-	answer += to_string(vecNums[0]);
-	answer += " ";
-	answer += to_string(vecNums[vecNums.size() - 1]);
-
-	return answer;
+/*
+일단 다 숫자로 변환해서.. Sort 때리고, 다시 string으로 변환해야겠지?
+*/
+string solution(string s) 
+{
+    int iLength = s.length();
+    vector<int> vecNums = {};
+    string strTemp = {}, answer = {};
+    
+    for(int i=0; i<iLength; ++i)
+    {
+        if(s[i] != ' ')
+            strTemp += s[i];
+        else
+        {
+            int iTemp = stoi(strTemp);
+            strTemp = "";
+            vecNums.push_back(iTemp);
+        }
+    }
+    
+    int iTemp = stoi(strTemp);
+    strTemp = "";
+    vecNums.push_back(iTemp);
+    
+    sort(vecNums.begin(), vecNums.end());
+    
+    answer += to_string(vecNums[0]);
+    answer += " ";
+    answer += to_string(vecNums[vecNums.size()-1]);
+    
+    return answer;
 }
