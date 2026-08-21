bfb6f1348acc8eb0941c80128e4ee0b44400d4ca
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-21 17:54:14 +0900

    [level 1] Title: 추억 점수, Time: 0.95 ms, Memory: 5.11 MB -BaekjoonHub

--- C++/프로그래머스/1/176963. 추억 점수/추억 점수.cpp
+++ C++/프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -1,52 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <map>
-#include <algorithm>
 #include <string>
-#include <list>
-#include <set>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-/*
-첫 방향 -> 내려 가기만
-두번째 방향 -> 옆으로 가기만
-세번째 방향 -> 대각선 왼쪽 위로 가기만.
-
-이후 반복.
-*/
-
 vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
-	vector<int> answer;
-	map<string, int> mapCost = {};
-
-	for (int i = 0; i < name.size(); ++i)
-	{
-		mapCost.emplace(name[i], yearning[i]);
-	}
-
-	for (int i = 0; i < photo.size(); ++i)
-	{
-		answer.push_back(0);
-		for (int j = 0; j < photo[i].size(); ++j)
-		{
-			answer[i] += mapCost[photo[i][j]];
-		}
-	}
-
-	return answer;
-}
-
-int main(void* pArg)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	solution({ "may", "kein", "kain", "radi" }, { 5, 10, 1, 3 },
-		{ {"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"},{"kon", "kain", "may", "coni"} });
-
-	return 0;
+    vector<int> answer;
+    map<string, int> map = {};
+    int iCount = 0;
+    
+    for(auto& iter : name)
+    {
+        map[iter] = yearning[iCount];
+        iCount++;
+    }
+    
+    int iRowSize = photo.size();
+    
+    for(int i=0; i<iRowSize; ++i)
+    {
+        int iColSize = photo[i].size(); 
+        int iSum = 0;
+        
+        for(int j=0; j<iColSize; ++j)
+        {
+            iSum += map[ photo[i][j] ];    
+        }
+        
+        answer.push_back(iSum);
+    }
+    
+    
+    return answer;
 }
