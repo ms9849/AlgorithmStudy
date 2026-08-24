4153d2c489f2217f89fcf3176d539a4c1b053400
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-24 13:14:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.24 ms, Memory: 4.57 MB -BaekjoonHub

--- C++/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ C++/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,54 +1,63 @@
 #include<vector>
-#include<queue>
+#include <queue>
 using namespace std;
 
+/*
+1. BFS로 풀어나가야 할듯 함. DFS로 하면 경우의 수가 너무 많아진다.
+2. 도착하지 못하는 경우도 있다. 이 경우 예외처리에 조심할 것.
+3. 맵의 크기는 직사각형일 수 있다. 
+-> BFS 하기전에 미리 row col 사이즈 체크해둘 것.
+*/
+
 int iVisited[101][101] = {};
-int solution(vector<vector<int> > maps)
+
+void BFS(const vector<vector<int>>& maps, int& answer)
 {
-	int answer = -1;
+    int iRowSize = maps.size() - 1;
+    int iColSize = maps[0].size() - 1;
+    
+    vector<pair<int,int>> vecDirs = {
+        {1,0},
+        {0,1},
+        {-1,0},
+        {0,-1}
+    };
+    
+    //(x,y), depth 순.
+    queue<pair<pair<int,int>,int>> q = {};
+    //시작 위치.
+    q.push(make_pair(make_pair(0,0), 1)); 
+    
+    while(!q.empty())
+    {
+        pair<int,int> CurPos = q.front().first;
+        int iCurDepth = q.front().second;
+        q.pop();
+        
+        if((CurPos.first == iRowSize) && (CurPos.second == iColSize) && (answer == -1 || iCurDepth < answer)) 
+        {
+            answer = iCurDepth;
+        }
+        
+        for(int i=0; i<4; ++i)
+        {
+            pair<int,int> Dir = vecDirs[i];
+            //그 방향이 방문한 적이 없다면, 그리고 범위 내에 있다면.
+            if(CurPos.first + Dir.first >= 0 && CurPos.first + Dir.first <= iRowSize && CurPos.second + Dir.second >= 0 && CurPos.second + Dir.second <= iColSize && 0 == iVisited[CurPos.first + Dir.first][CurPos.second + Dir.second] && maps[CurPos.first + Dir.first][CurPos.second + Dir.second] != 0)
+            {
+                //방문 처리.
+                iVisited[CurPos.first + Dir.first][CurPos.second + Dir.second] = 1;
+                q.push(make_pair(make_pair(CurPos.first + Dir.first, CurPos.second + Dir.second), iCurDepth + 1));
+            }
+        }
+    }
+}
 
-	//y
-	int iN = maps.size() - 1;
-	//x
-	int iM = maps[0].size() - 1;
-
-	queue<pair<pair<int, int>, int>> q = {};
-	vector<pair<int, int>> vecDirs = {
-		{1,0},
-		{0,1},
-		{-1,0},
-		{0,-1}
-	};
-	q.push( {{0,0}, 1} );
-	iVisited[0][0] = 1;
-
-	while (!q.empty())
-	{
-		pair<pair<int, int>, int> Node = q.front();
-		q.pop();
-
-		if (Node.first.first == iN && Node.first.second == iM)
-		{
-			return Node.second;
-		}
-
-
-		for (auto& iter : vecDirs)
-		{
-			int iNextY = Node.first.first + iter.first;
-			int iNextX = Node.first.second + iter.second;
-
-			if (iNextX >= 0 && iNextX <= iM &&
-				iNextY >= 0 && iNextY <= iN &&
-				iVisited[iNextY][iNextX] == 0 &&
-				maps[iNextY][iNextX] != 0)
-			{
-				iVisited[iNextY][iNextX] = 1;
-				q.push({{ iNextY, iNextX }, Node.second + 1 });
-			}
-		}
-
-	}
-
-	return answer;
+int solution(vector<vector<int>> maps)
+{
+    int answer = -1;
+ 
+    BFS(maps, answer);
+    
+    return answer;
 }
