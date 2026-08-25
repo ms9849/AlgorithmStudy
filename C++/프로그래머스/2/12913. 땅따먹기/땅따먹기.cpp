eafc45a5bce7c9479dd43e98848e5f72aa59d5c0
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-25 16:16:06 +0900

    [level 2] Title: 땅따먹기, Time: 4.62 ms, Memory: 27.7 MB -BaekjoonHub

--- C++/프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ C++/프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -2,36 +2,43 @@
 #include <vector>
 using namespace std;
 
-int iDp[100001][5] = {};
-//열당 10만번. DP로 풀이 가능
-int solution(vector<vector<int>> land)
+/*
+무조건 DP로 풀어야함. 
+완탐은 불가능.
+
+항상 최대값을 얻어와야하는 그리드는 아님. -> 이전 조건이 발목을 잡기 때문.
+dp로 매 순간 기록해서 그 순간의 최적값들을 전부 모아야한다.
+*/
+
+int iDp[100001][4];
+
+int solution(vector<vector<int> > land)
 {
-	int iRowSize = land.size();
+    int answer = 0;
 
-	for (int i = 0; i < 4; ++i)
-	{
-		iDp[0][i] = land[0][i];
-	}
-
-	for (int i = 1; i < iRowSize; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			for (int k = 0; k < 4; ++k)
-			{
-				if (j != k)
-				{
-					iDp[i][j] = max(iDp[i - 1][k] + land[i][j], iDp[i][j]);
-				}
-			}
-		}
-	}
-
-	int iMax = -1;
-	for (int i = 0; i < 4; ++i)
-	{
-		iMax = max(iMax, iDp[iRowSize - 1][i]);
-	}
-
-	return iMax;
+    for(int i=0; i<4; ++i)
+        iDp[0][i] = land[0][i];
+    
+    for(int i=1; i<land.size(); ++i)
+    {
+        for(int j=0; j<4; ++j)
+        {
+            int iMax = -1;
+            
+            for(int k=0; k<4; ++k)
+            {
+                 if(k != j)
+                      iMax = max(iMax, iDp[i-1][k]);      
+            }
+            
+            iDp[i][j] = iMax + land[i][j];
+        }
+    }
+    
+    for(int i=0; i<4; ++i)
+    {
+        answer = max(answer, iDp[land.size()-1][i]);
+    }
+    
+    return answer;
 }
