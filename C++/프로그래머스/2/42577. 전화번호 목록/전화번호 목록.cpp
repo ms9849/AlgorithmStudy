d6b0ce51219960bd42b487fd34677b88b8345031
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-25 17:09:11 +0900

    [level 2] Title: 전화번호 목록, Time: 122.12 ms, Memory: 48.8 MB -BaekjoonHub

--- C++/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ C++/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,34 +1,36 @@
 #include <string>
 #include <vector>
-#include <map>
-#include <algorithm>
+#include <unordered_map>
+
 using namespace std;
+/*
+100만개의 원소가 존재할 수 있으니까..
+단순 2중 for문은 아님. 100억이 넘나? 아무튼..
 
-
+map에 다 미리 등록해놓고, 2중 for문으로 체크하는건 되겠다.
+100만번 * 20이니까. 
+*/
 bool solution(vector<string> phone_book) {
-	bool answer = true;
-	map<string, bool> mapCache = {};
-
-	sort(phone_book.begin(), phone_book.end(), [](string lhs, string rhs)->bool {
-		return lhs.length() < rhs.length();
-	});
-
-
-	for (auto& iter : phone_book)
-		mapCache[iter] = true;
-
-
-	for (auto& str : phone_book)
-	{
-		string strTemp = {};
-		for (int i = 0; i < str.length(); ++i)
-		{
-			strTemp += str[i];
-
-			if (mapCache[strTemp] == true && strTemp != str)
-				return false;
-		}
-	}
-
-	return true;
+    bool answer = true;
+    unordered_map<string, int> um = {};
+    
+    for(auto& iter : phone_book)
+        um[iter]++;
+    
+    int iSize = phone_book.size();
+    for(int i=0; i<iSize; ++i)
+    {
+        string strTemp = "";
+        int iLength = phone_book[i].length();
+        
+        for(int j=0; j<iLength-1; ++j)
+        {
+            strTemp += phone_book[i][j];
+        
+            if(um.find(strTemp) != um.end())
+                return false;
+        }
+    }
+    
+    return true;
 }
