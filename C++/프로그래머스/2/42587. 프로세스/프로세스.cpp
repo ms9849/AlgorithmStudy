7f6680018d6a7ab3c20b29a33a321346ecdf9708
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-25 15:55:04 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

--- C++/프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ C++/프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,57 +1,55 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <map>
-#include <algorithm>
 #include <string>
-#include <list>
-
-//어쨌든 가장 긴 녀석은 곱해줘야 함..
-// 다 스왑이 가능하다는거니까?..
-// 8개면
-// 0번쨰 인덱스 * 4번째 인덱스?
-
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
+/*
+pq는 불가능. 시뮬레이션이 필요하니까, 내림차순으로 정렬된 vector 하나와
+실제 queue가 필요함.
+*/
 int solution(vector<int> priorities, int location) {
-	int iCount = 0;
-	vector<pair<int,bool>> q = {};
-
-	int iIdx = 0;
-	for (auto& iter : priorities)
-	{
-		if (iIdx != location)
-			q.push_back({ iter, false });
-		else
-			q.push_back({ iter, true });
-
-		iIdx++;
-	}
-
-	while (!q.empty())
-	{
-		pair<int, bool> Node = q.front();
-		int iMax = 0;
-
-		for (auto& iter : q)
-		{
-			if(iMax < iter.first)
-				iMax = iter.first;
-		}
-
-		q.erase(q.begin());
-
-		if (Node.first == iMax)
-		{
-			iCount++;
-
-			if (Node.second == true)
-				return iCount;
-		}
-		else
-		{
-			q.push_back(Node);
-		}
-	}
+    int answer = 0;
+    // (우선순위, 원본 인덱스)
+    queue<pair<int,int>> q = {};
+    
+    for(int i=0; i<priorities.size(); ++i)
+    {
+        // 원본 위치를 기억하게 하면서 삽입.
+        q.push(make_pair(priorities[i],i));
+    }
+    
+    sort(priorities.begin(), priorities.end(), [](int lhs, int rhs)->bool {
+        return lhs > rhs;
+    });
+    
+    int iCount = 1;
+    while(!q.empty())
+    {
+        int iPrior = priorities[0];
+        
+        while(1)
+        {
+            pair<int,int> p = q.front();
+            
+            if(p.first == iPrior)
+            {              
+                priorities.erase(priorities.begin());
+                q.pop();
+                if(p.second == location)
+                    return iCount;
+                iCount++;
+                break;
+            }
+            else
+            {
+                q.push(p);   
+                q.pop();
+            }
+        }
+    }
+    
+    
+    
+    return answer;
 }
