c33a6b38a59680f8ec4d37a8a17fc0453d9a8309
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-25 15:42:31 +0900

    [level 2] Title: 피로도, Time: 0.56 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/2/87946. 피로도/피로도.cpp
+++ C++/프로그래머스/2/87946. 피로도/피로도.cpp
@@ -3,52 +3,35 @@
 
 using namespace std;
 
-//가거나, 가지 않거나..
-//최대가 나오려면 계속 탐사를 하긴 해야겠지?..
-//dp로도 풀이 가능해보이지만 완탐이 가장 직관적일 것.
+int iVisited[5001] = {};
 
-int iVisited[10] = {};
-int iMaxDungeons = {};
-int iNumDungeons = {};
-vector<vector<int>> vecDungeons = {};
-/* 종료 조건 */
-// 1. 모든 던전을 탐험함
-// 2. 더 이상 피로도 조건에 맞는 던전이 없음.
-
-void Recursion(int iCurPower, int iCurDungeons)
+void DFS(int& k, const vector<vector<int>>& dungeons, int iCurDepth, int& iMaxDepth)
 {
-    bool isRaidable = { false };
-    for(int i=0; i<iNumDungeons; ++i)
-    {
-        //최소 필요 피로도
-        int iRequirePower = vecDungeons[i][0];
-        //사용 피로도
-        int iUsePower = vecDungeons[i][1]; 
-        
-        if(iVisited[i] == 0 &&
-          iCurPower >= iRequirePower &&
-          iCurPower >= iUsePower)
-        {
-            isRaidable = true;
-            
-            iVisited[i] = 1;
-            Recursion(iCurPower - iUsePower, iCurDungeons + 1);
-            iVisited[i] = 0;
-        }
-    }
+    if(iMaxDepth < iCurDepth)
+        iMaxDepth = iCurDepth;
     
-    if(false == isRaidable)
+    int iSize = dungeons.size();
+    
+    for(int i=0; i<iSize; ++i)
     {
-        iMaxDungeons = iMaxDungeons > iCurDungeons ? iMaxDungeons : iCurDungeons;
+        if(0 == iVisited[i] && k >= dungeons[i][0])
+        {
+            iVisited[i] = 1;
+            k -= dungeons[i][1];
+            
+            DFS(k, dungeons, iCurDepth + 1, iMaxDepth);
+            
+            iVisited[i] = 0;
+            k += dungeons[i][1];
+        }
     }
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    int iAnswer = { -1 };
-    vecDungeons = dungeons;
-    iNumDungeons = dungeons.size();
+    int answer = -1;
     
-    Recursion(k, 0);
+    DFS(k,dungeons, 0, answer);
     
-    return iMaxDungeons;
+    
+    return answer;
 }
