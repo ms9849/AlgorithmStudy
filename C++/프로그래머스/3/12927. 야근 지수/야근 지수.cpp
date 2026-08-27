2420b72d3e79a83aa823124e38a73839dea64fec
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-27 15:16:01 +0900

    [level 3] Title: 야근 지수, Time: 15.31 ms, Memory: 4.57 MB -BaekjoonHub

--- C++/프로그래머스/3/12927. 야근 지수/야근 지수.cpp
+++ C++/프로그래머스/3/12927. 야근 지수/야근 지수.cpp
@@ -3,38 +3,36 @@
 #include <queue>
 using namespace std;
 
+/*
+works가 2만이니까... 최대값으로 10억까지 나올 수 있음.
+long long을 써야..하긴 하지.
+
+1. 큰 수부터 줄여야함 -> Pq로 push & pop 할까?
+*/
 long long solution(int n, vector<int> works) {
-	long long answer = 0;
-	priority_queue<int, vector<int>, less<int>> pq = {};
+    long long answer = 0;
+    int iSize = works.size();
+    priority_queue<int, vector<int>, less<int>> pq = {};
+    
+    for(int i=0; i<iSize; ++i)
+        pq.push(works[i]);
+    
+    while(n != 0 && pq.top() > 0)
+    {
+        int iNum = pq.top();
+        pq.pop();
 
-	for (auto& iter : works)
-	{
-		pq.push(iter);
-	}
-
-	while (n != 0)
-	{
-		int iNum = pq.top();
-		pq.pop();
-
-		iNum--;
-		n--;
-
-		pq.push(iNum);
-	}
-
-	while (!pq.empty())
-	{
-		int iNum = pq.top();
+        pq.push(--iNum);
+        n--;
+    }
+    
+    while(!pq.empty())
+    {
+        int iNum = pq.top();
+        pq.pop();
         
-        if (iNum < 0)
-			return 0;
-        
-		pq.pop();
-
-		answer += iNum* iNum;
-	}
-
-	return answer;
-
+        answer += iNum * iNum;
+    }
+    
+    return answer;
 }
