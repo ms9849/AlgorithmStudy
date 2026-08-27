ff044f801cae0d9491c78867579c1576ec0b2e13
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-27 15:32:35 +0900

    [level 3] Title: 이중우선순위큐, Time: 6.85 ms, Memory: 11.3 MB -BaekjoonHub

--- C++/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ C++/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,80 +1,52 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <map>
+#include <set>
+
 using namespace std;
 
-
+/*
+넣을때마다 Sort...는 안되겠지.
+최대, 최소값을 보관하고 있는 두 개의 자료구조가 필..요하겠지.
+map도 괜찮을까? set도 괜찮다곤 했던 것 같은데.
+*/
 vector<int> solution(vector<string> operations) {
-
-	vector<int> answer;
-
-	map<int, int> mapCounter = {};
-	priority_queue<int, vector<int>, less<int>> pqMax = {};
-	priority_queue<int, vector<int>, greater<int>> pqMin = {};
-
-	for (auto& iter : operations)
-	{
-		char cCmd = iter[0];
-		int  iNum = stoi(iter.substr(iter.find(' ') + 1));
-
-		switch (cCmd)
-		{
-		case 'D':
-			if (iNum == -1)
-			{
-				if (!pqMin.empty() && mapCounter[pqMin.top()] > 0)
-				{
-					mapCounter[pqMin.top()]--;
-					pqMin.pop();
-				}
-
-				while (!pqMin.empty() && mapCounter[pqMin.top()] == 0)
-					pqMin.pop();
-				while (!pqMax.empty() && mapCounter[pqMax.top()] == 0)
-					pqMax.pop();
-			}
-			else if (iNum == 1)
-			{
-				if (!pqMax.empty() && mapCounter[pqMax.top()] > 0)
-				{
-					mapCounter[pqMax.top()]--;
-					pqMax.pop();
-				}
-
-				while (!pqMin.empty() && mapCounter[pqMin.top()] == 0)
-					pqMin.pop();
-				while (!pqMax.empty() && mapCounter[pqMax.top()] == 0)
-					pqMax.pop();
-			}
-
-			break;
-
-		case 'I':
-			pqMax.push(iNum);
-			pqMin.push(iNum);
-			
-			mapCounter[iNum]++;
-			break;
-		}
-	}
-
-	if (pqMin.empty() || pqMax.empty())
-		return { 0,0 };
-
-	else
-	{
-		while (mapCounter[pqMax.top()] == 0)
-			pqMax.pop();
-
-		answer.push_back(pqMax.top());
-
-		while (mapCounter[pqMin.top()] == 0)
-			pqMin.pop();
-		answer.push_back(pqMin.top());
-	}
-
-	return answer;
+    vector<int> answer = {};
+    multiset<int> s = {};
+    int iOperSize = operations.size();
+    
+    for(int i=0; i<iOperSize; ++i)
+    {
+        string strOper = operations[i];
+        string strNum = {};
+        
+        switch(strOper[0])
+        {
+            case 'I':
+                for(int i=2; i<strOper.size(); ++i)
+                    strNum += strOper[i];
+                s.insert(stoi(strNum));
+                break;
+            case 'D':
+                
+                if(strOper[2] == '-')
+                    s.erase(*s.begin());   
+                else
+                    s.erase(*s.rbegin());
+                
+                break;
+        }
+    }
+    
+    if(!s.empty())
+    {
+        answer.push_back(*s.rbegin());
+        answer.push_back(*s.begin());
+    }
+    else
+    {
+        answer.push_back(0);
+        answer.push_back(0);
+    }
+    
+    return answer;
 }
-
