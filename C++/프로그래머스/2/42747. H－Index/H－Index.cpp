627e0e7f4e03e571b5635fd1f5c41e41c8b77c6b
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-31 14:24:56 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.64 MB -BaekjoonHub

--- C++/프로그래머스/2/42747. H－Index/H－Index.cpp
+++ C++/프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -4,26 +4,36 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    int iAnswer = 0;
+    int answer = 0;
+    int iMid = 0;
+    int iMin = 0;
+    int iMax = citations.size()-1;
+    int iSize = citations.size();
+    
     sort(citations.begin(), citations.end());
     
-    int iMax = citations[citations.size()-1];
-    
-    for(int i=0; i<iMax; ++i)
+    while(iMin <= iMax)
     {
-        int iHCount = 0;
-        for(int j=0; j<citations.size(); ++j)
+        iMid = (iMin + iMax) / 2;
+        // ival번 이상 인용된 논문이,
+        int iVal = citations[iMid];
+        // iSize - iMid 개 존재함.
+        int iCount = iSize - iMid;
+        // 3개 이상의 논문이 1회 이상 인용되었다면..
+        // 논문 3(h)개는 안되지.
+        if(iCount <= iVal)
         {
-            if(citations[j] >= i)
-                iHCount++;
+            iMax = iMid -1;
+            answer = iCount;
+        }
+        else
+        {
+            iMin = iMid+1;
         }
         
-        if(iHCount >= i)
-            iAnswer = i;
-        else
-            break;
     }
     
     
-    return iAnswer;
+    
+    return answer;
 }
