dab0bee03dce722e3c6d2853373364d77b9858b1
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-08-31 13:59:49 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

--- C++/프로그래머스/1/42748. K번째수/K번째수.cpp
+++ C++/프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,36 +1,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
+    int iCmdSize = commands.size();
     
-    int iSize = commands.size();
-    
-    for(int i=0; i<iSize; ++i)
+    for(int i=0; i<iCmdSize; ++i)
     {
-        //어디서부터 어디까지 자를래?
-        int iStart = commands[i][0] - 1;
-        int iEnd = commands[i][1] - 1;
-        //k번째 수.
-        int iK = commands[i][2] - 1;
+        int iFrom = commands[i][0];
+        int iTo = commands[i][1];
+        int iIdx = commands[i][2];
         
-        //자른 숫자들을 저장하기 위한 벡터 컨테이너.
         vector<int> vecNums = {};
-        for(int j=iStart; j<=iEnd; ++j)
-        {
+        
+        for(int j=iFrom-1; j<iTo; ++j)
             vecNums.push_back(array[j]);
-        }
         
-        sort(vecNums.begin(), vecNums.end(), [](int ilhs, int irhs) -> bool         {
-            return ilhs < irhs;
-        });
+        sort(vecNums.begin(), vecNums.end());
         
-        answer.push_back(vecNums[iK]);
+        answer.push_back(vecNums[iIdx-1]);
     }
-    
     
     return answer;
 }
