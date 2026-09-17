9f514aeeba6751248962f3f4412716677d669b28
ms9849 <65911657+ms9849@users.noreply.github.com>
2026-09-17 16:11:37 +0900

    [level 2] Title: 숫자 변환하기, Time: 4.43 ms, Memory: 7.55 MB -BaekjoonHub

--- C++/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ C++/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -3,43 +3,57 @@
 
 using namespace std;
 
+/*
+몇번의 연산을 통해 x에서 목표 y로 변환이 가능한지 확인하는 문제.
+아마 DP일 것. +, *2, *3 첫 인덱스만 계산해놓고 해도?..
+*/
+
+int iDp[1000001] = {};
+
 int solution(int x, int y, int n) {
+    int answer = 0;
+
+    if(x+n <= y)
+        iDp[x+n] = 1;
+    if(x*2 <= y)
+        iDp[x*2] = 1;    
+    if(x*3 <= y)
+        iDp[x*3] = 1;
     
-	if (x == y)
-		return 0;
+    for(int i=x; i<=y; ++i)
+    {
+        if(iDp[i] != 0)
+        {
+            if(i+n <= y)
+            {
+                //무언가 이미 도달할 수 있는 방법이 있다면?
+                if(iDp[i+n] != 0)
+                    iDp[i+n] = min(iDp[i+n], iDp[i] + 1);
+                else
+                    iDp[i+n] = iDp[i] + 1;   
+            }
+            if(i*2 <= y)
+            {
+                if(iDp[i*2] != 0)
+                    iDp[i*2] = min(iDp[i*2], iDp[i] + 1);
+                else
+                    iDp[i*2] = iDp[i] + 1;
+            }
+
+            if(i*3 <= y)
+            {
+                if(iDp[i*3] != 0)       
+                    iDp[i*3] = min(iDp[i*3], iDp[i] + 1);
+                else
+                    iDp[i*3] = iDp[i] + 1;   
+            }   
+        }
+    }
+
+    if(x == y)
+        return 0;
+    if(iDp[y] == 0)
+        return -1;
     
-	int answer = 0;
-
-	int iDp[1000001] = {};
-	iDp[x] = 0;
-	if (x + n <= y)
-		iDp[x + n] = 1;
-	if(x*2 <= y)
-		iDp[x * 2] = 1;
-	if(x*3 <= y)
-		iDp[x * 3] = 1;
-	//-1로 초기화
-
-	for (int i = x+1; i < y; ++i)
-	{
-		if ((i + n <= y) && iDp[i + n] != 0 && iDp[i] != 0)
-			iDp[i + n] = min(iDp[i+n], iDp[i] + 1);
-		else if((i + n <= y) && iDp[i + n] == 0 && iDp[i] != 0)
-			iDp[i + n] = iDp[i] + 1;
-
-		if ((i * 2 <= y) && iDp[i * 2] != 0 && iDp[i] != 0 )
-			iDp[i * 2] = min(iDp[i*2], iDp[i] + 1);
-		else if(i * 2 <= y && iDp[i * 2] == 0 && iDp[i] != 0)
-			iDp[i * 2] = iDp[i] + 1;
-
-		if (iDp[i] != 0 && (i * 3 <= y) && iDp[i * 3] != 0)
-			iDp[i * 3] = min(iDp[i * 3], iDp[i] + 1);
-		else if (i * 3 <= y && iDp[i * 3] == 0 && iDp[i] != 0)
-			iDp[i * 3] = iDp[i] + 1;
-	}
-
-	if (iDp[y] == 0)
-		return -1;
-
-	return iDp[y];
+    return iDp[y];
 }
